2c45f8184062034ca6321c6f7b23126f893aa553
Tran Mai Uyen Nhi <24730053@ms.uit.edu.vn>
2026-08-17 13:28:14 +0700

    Remove getBaseUrl function and use environment variable

--- work-hub_pmapp/src/lib/rpc.ts
+++ work-hub_pmapp/src/lib/rpc.ts
@@ -2,12 +2,5 @@ import { hc } from "hono/client";
 
 import { AppType } from "@/app/api/[[...route]]/route";
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") {
-    return window.location.origin;
-  }
-  return process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
-};
-
-export const client = hc<AppType>(getBaseUrl());
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
 
